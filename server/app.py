47a59092c5b433f02d2d5ce44abb9a0817f18e9b
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-05-26 16:24:48 +0300

    commit post-patch-delete-api-lab

--- server/app.py
+++ server/app.py
@@ -37,13 +37,67 @@ def baked_goods_by_price():
         bg.to_dict() for bg in baked_goods_by_price
     ]
     return make_response( baked_goods_by_price_serialized, 200  )
-   
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
+
+@app.route('/baked_goods', methods=['POST'])
+def new_baked_good():
+    name = request.form.get('name')
+    price = request.form.get('price')
+
+
+    if not name or not price:
+        return jsonify({"error": "Missing name or price"}), 400
+
+    try:
+        price = float(price)
+    except ValueError:
+        return jsonify({"error": "Price must be a number"}), 400
+
+    baked_good = BakedGood(name=name, price=price)
+    db.session.add(baked_good)
+    db.session.commit()
+
+    return jsonify(baked_good.to_dict()), 201
+
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
+def bakery_change_name(id):
+    bakery = db.session.get(Bakery, id)
+    if not bakery:
+        return jsonify({"error": "Bakery not found"}), 404
+
+    if request.method == 'PATCH':
+        name = request.form.get('name')
+        if name:
+            bakery.name = name
+            db.session.commit()
+
+        return jsonify(bakery.to_dict()), 200
+
+    return jsonify(bakery.to_dict()), 200
+
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = db.session.get(BakedGood, id)
+
+    if not baked_good:
+        return jsonify({"error": "Baked good not found"}), 404
+
+    db.session.delete(baked_good)
+    db.session.commit()
+
+    return jsonify({"message": "record successfully deleted"}), 200
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
